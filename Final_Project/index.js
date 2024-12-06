9dbb6aad86907a164a92f18e0a54ac964b193354
Jordan Pinkava <jpinkava@umd.edu>
2024-12-05 20:42:33 -0500

    cleaning up code

--- Final_Project/index.js
+++ Final_Project/index.js
@@ -8,7 +8,7 @@
 
 
 
-//annyang code from asn 2 -- to be converted to code for this project
+//annyang code 
 if (annyang) {
    const commands = {
      'navigate to :page' : (page) => {
@@ -20,8 +20,6 @@ if (annyang) {
        if (pages[pageChoice]) {
          window.location.href = pages[pageChoice];};},
 
-
-       //for selecting filters--this is pretty much good, just need to integrate with HTML (the ID's)
       'select :filter': (filter) => {
          const filterChoice = filter.toLowerCase();
          const filters = {
@@ -44,20 +42,6 @@ if (annyang) {
       'done' : () =>{
          document.getElementById('submit').click();
       }
-      
-      //might need to have complimentary functions
-      //search for term and county code
-      //ignore for now -- will implement if it isn't too difficult/confusing
-   //   'search :term': (term) => {
-   //       },
-   //    //select drop down for event and family friendly (could this also get complicated?)
-   //    'select :item': (item) => {
-   //       },
-   //    //date for date -- do we need voice command for all (i feel like this might be complicated)
-
-   //    'date :input': (input) => {
-   //       }
-
    };
  
    annyang.addCommands(commands);
@@ -77,48 +61,5 @@ if (annyang) {
    document.getElementById('turnOn').addEventListener('click', start);
    document.getElementById('turnOff').addEventListener('click', stop);
  }
- 
-//anyyang html for reference
-//  <!--audio library section-->
-      //def need the src here
-//       <script src="//cdnjs.cloudflare.com/ajax/libs/annyang/2.6.1/annyang.min.js"></script>
-      //this would go on about page and maybe on home page too for cleanliness?       
-//       <div id = "audio">
-//          <h2>Navigate the webpage using audio</h2>
-//          <p>Ensure mic is on to use this feature</p>
-//          <li>Say "hello" to get greeted back</li>
-//          <li>Say "change the color to *color* to change page color"</li>
-//          <li>Say "navigate to *page name* to naivgate to a different page"</li>
-//          <li>Say "lookup *stock* to lookup a stock</li>
-//          <button id = "turnOn">Turn On Listening</button>
-//          <button id = "turnOff">Turn Off Listening</button>
-//       </div>
-
-
-//input validatpr JS code from lab 2 (integrated into html)
-// <!--Box 2--validating text-->
-//       <div class = "validate">
-//          <h2 id = "validation">Validate Text</h2>
-//          <form name = "verifyForm" onsubmit="return validateText();">
-//             <label for = "textInput">Text Tester</label>
-//             <input type = "text" placeholder = "Text" id = "textInput">
-//             <button type = "submit" >TEST ME</button>
-//          </form>
-//          <script type = "text/javascript">
-//             var validation = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-//             function validateText() {
-//                   const text = document.verifyForm.textInput.value;
-//                   if(text === "") {
-//                      alert('Enter Text, please');
-//                      return false;
-//                   }
-//                   if (validation.test(text)) {
-//                      alert('This Text is Not Valid');
-//                      return false;
-//                   }
-//                   alert('This Text is valid!');
-//                   return true;
-//                }
-//          </script></script>
 
 
