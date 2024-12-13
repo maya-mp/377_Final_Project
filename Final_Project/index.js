be08aee8799f2f9ce6ed3e4922761efa6ec278db
Angad Sharma <Angads2005@gmail.com>
2024-12-13 01:34:57 -0500

    created mock endpoints

--- Final_Project/index.js
+++ Final_Project/index.js
@@ -4,8 +4,14 @@ const app = express()
 const port = 3000
 app.use(express.static(__dirname + '/public'))
 
-app.get('/names
+app.get('/Datasets', (req, res) => {
+   console.log('Attempting to get all customers.')
+})
 
+app.post('/Dataset', (req, res) => {
+   console.log('Attempting to add another dataset.')
+})
+  
 app.listen(port, () => {
    console.log('App works')
 })
